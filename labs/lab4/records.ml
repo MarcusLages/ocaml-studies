9b0e79e1b095b21d9cad9b4c7f8353fc2fee211a
MarcusLages <marcusvlages@gmail.com>
2025-02-06 14:17:14 -0800

    Added documentation to lab4 structure and initial error handling

--- labs/lab4/records.ml
+++ labs/lab4/records.ml
@@ -1,23 +1,40 @@
-type record = {firstname: string; lastname: string; score: int}
+(** [record] type to store record information from file *)
+type record = {
+    firstname: string;
+    lastname: string;
+    score: int
+}
 
-let rec get_records_list ic = 
+(** [make_record] creates a record by using the given inputs. *)
+let make_record firstname lastname score =
+    {firstname; lastname; score}
+
+(** [get_records] is used to read records from an [in_channel],
+    and then parse and store them into a list of [record]'s *)
+let rec get_records ic = 
     let aux acc = ()
     in
     aux []
 
-let parse line = []
+(** [parse] parses a line into a [record] *)
+let parse line = make_record "asdf" "asdfas" 0
 
+(** [sort_records] sorts the [record]s ordered by their score
+    in decreasing order *)
 let sort_records lst = []
 
-let write_records lst = ()
+(** [output_records] outputs the [records] to the screen *)
+let output_records lst = ()
 
+(** Initial function to run program and check for correct arguments
+    and errors *)
 let () =
     if Array.length Sys.argv = 1 then
         Printf.fprintf stderr "usage: %s <file>\n" Sys.argv.(0)
     else
         try
-            let records = get_records_list (open_in Sys.argv.(1))
+            let records = get_records (open_in Sys.argv.(1))
             in
-            write_records @@ sort_records records
+            output_records @@ sort_records records
         with
         | Sys_error msg -> Printf.fprintf stderr "%s\n" msg
